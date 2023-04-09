cef02c6191edb7b25dd20bc3e68aa62629e5b36c
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-04-09 12:54:06 +0500

    dddd

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// let name1 = 'Nurmuhammad';
+// let name2 = 'Yorov';
+// let age1 = 35;
+// let age2 = 'yoshli';
+// let bob = ' bo`lajak boboy dasturchi';
+// console.log (name1 + name2 + age1 + age2 + bob);
+
+// const person = {
+//     name3: 'Muhammad',
+//     age3: 75,
+//     lastname: 'Yorov'
+// }
+
+// module.exports = person;
+// module.exports = person.age3; bittalab chaqirish
+
+// function num (a,b) {
+//     return a + b
+// }
+
+// module.exports.num = num;
+
---
+++ app2.js
@@ -0,0 +1,21 @@
+
+
+// const user = require('./app.js');
+
+// console.log(user);
+
+// (function (modul, exports, require) {
+
+//     const user = require('./app.js')
+
+//     console.log(user);
+    
+// })()
+
+
+
+const path = require ('path');
+
+// console.log(__filename); //FILNI NOMINI QAYTARADI
+
+console.log(path.dirname (__filename)); 
